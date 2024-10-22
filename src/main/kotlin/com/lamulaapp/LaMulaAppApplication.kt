4c0c4be8dd45515ac89ba390e5d6ab3744ef6a66
Vanessa Requejo Porras <vnporras1028@gmail.com>
2024-10-22 18:08:43 -0500

    first commit

---
+++ src/main/kotlin/com/lamulaapp/LaMulaAppApplication.kt
@@ -0,0 +1,11 @@
+package com.lamulaapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LaMulaAppApplication
+
+fun main(args: Array<String>) {
+    runApplication<LaMulaAppApplication>(*args)
+}
---
+++ src/test/kotlin/com/lamulaapp/LaMulaAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lamulaapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LaMulaAppApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
